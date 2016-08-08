d32da259a5305d1c067e54600f1f87c7a345b0b6
Amir Mingaliev <amir.mingaliev@flatstack.com>
2016-08-08 16:59:56 +0300

    Add auth_phone_codes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130319140714) do
+ActiveRecord::Schema.define(version: 20160808133309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: true do |t|
+  create_table "auth_phone_codes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "phone_code"
+    t.datetime "expire_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "auth_phone_codes", ["user_id"], name: "index_auth_phone_codes_on_user_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
