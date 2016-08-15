134d579723a864090798ab27669e111434a526bb
Amir Mingaliev <amir.mingaliev@flatstack.com>
2016-08-15 13:04:17 +0300

    Restrict auth_phone_code serializable attributes

--- app/serializers/auth_phone_code_serializer.rb
+++ app/serializers/auth_phone_code_serializer.rb
@@ -1,5 +1,5 @@
 class AuthPhoneCodeSerializer < ApplicationSerializer
-  attributes :id, :phone_code, :expire_at, :phone_registered
+  attributes :id, :phone_registered
 
   def phone_registered
     object.user.present?
