7eb3c3bf150475744e905e0bf66bbb14eeb80001
Amir Mingaliev <amir.mingaliev@flatstack.com>
2016-08-17 12:29:14 +0300

    Rename auth_token to authentication_token

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += %i(password auth_token sms_code)
+Rails.application.config.filter_parameters += %i(password authentication_token sms_code)
