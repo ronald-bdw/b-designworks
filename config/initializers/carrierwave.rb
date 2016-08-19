fa7d2c8398f3c8537778920057bf104e27c250aa
Ildus Sadykov <ildus.sadykov@flatstack.com>
2016-08-19 14:09:05 +0300

    remove fog provider from CarrierWave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,8 @@
 CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
-    config.storage :file
+    config.storage = :file
   else
-    config.storage :fog
-    config.fog_provider = "fog/aws"
+    config.storage = :fog
     config.fog_credentials = {
       provider:              "AWS",
       aws_access_key_id:      ENV["S3_ACCESS_KEY"],
