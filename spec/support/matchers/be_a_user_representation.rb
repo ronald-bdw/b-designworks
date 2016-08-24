d697c2e498958cba1b586344be4e06f4e8bd6edf
Ildus Sadykov <ildus.sadykov@flatstack.com>
2016-08-24 11:29:25 +0300

    add first name and last name to user serializer

--- spec/support/matchers/be_a_user_representation.rb
+++ spec/support/matchers/be_a_user_representation.rb
@@ -2,6 +2,8 @@ RSpec::Matchers.define :be_a_user_representation do |user|
   match do |json|
     response_attributes = user.sliced_attributes %w(
       id
+      first_name
+      last_name
       authentication_token
       email
       phone_number
