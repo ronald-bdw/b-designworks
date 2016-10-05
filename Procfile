7d959df441ee136b0d75682b5ad06527ad22d13b
Amir Mingaliev <amir.mingaliev@flatstack.com>
2016-10-05 13:01:28 +0300

    Add delayed_job

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-web: bundle exec rails server thin -p $PORT -e $RACK_ENV
 release: bundle exec rake db:migrate
+web: bundle exec rails server thin -p $PORT -e $RACK_ENV
+worker:  bundle exec rake jobs:work
--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,7 @@ module RailsBaseApi
     # Disable default Rails headers which do not make sense in
     # API-only project (X-Frame-Options, X-XSS-Protection, X-Content-Type-Options)
     config.action_dispatch.default_headers = {}
+
+    config.active_job.queue_adapter = :delayed_job
   end
 end
