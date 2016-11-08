9de2c9f6584299a8b7d28bcd2bf557b92fe4e56a
Ildus Sadykov <ildus.sadykov@flatstack.com>
2016-11-08 10:26:51 +0300

    [#132225325] BE: show statuses of in-app purchases

--- spec/support/matchers/be_a_zendesk_user_representation.rb
+++ spec/support/matchers/be_a_zendesk_user_representation.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define :be_a_zendesk_user_representation do
   match do |json|
-    keys = %w(integrations notifications)
+    keys = %w(integrations notifications subscription_status)
 
     expect(json).to be
     expect(json).to include(*keys)
