8244f2e9049e72fd72f06186b0784da9d9062489
Amir Mingaliev <amir.mingaliev@flatstack.com>
2016-11-16 13:51:42 +0300

    Validates subscription plan name, update docs

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,8 @@
 class Subscription < ActiveRecord::Base
+  PLANS = %w(monthly trial).freeze
+
   belongs_to :user
 
   validates :plan_name, :expires_at, presence: true
+  validates :plan_name, inclusion: PLANS
 end
--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :subscription do
     user
-    plan_name "My plan"
+    plan_name "trial"
     expires_at "2016-11-01 12:18:08"
     active false
   end
