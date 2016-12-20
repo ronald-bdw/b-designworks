597ef8d0c4354d1a5d2bf2ab2441adc87382705b
Amir Mingaliev <amir.mingaliev@flatstack.com>
2016-12-20 18:45:21 +0300

    Serialize subscription status with plan name

--- spec/support/matchers/be_a_zendesk_user_representation.rb
+++ spec/support/matchers/be_a_zendesk_user_representation.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define :be_a_zendesk_user_representation do
   match do |json|
-    keys = %w(integrations notifications subscription_status)
+    keys = %w(integrations notifications subscription)
 
     expect(json).to be
     expect(json).to include(*keys)
