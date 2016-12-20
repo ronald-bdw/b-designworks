cbbe9864d52aaa9e54bc9b670ee75756dd12d275
Amir Mingaliev <amir.mingaliev@flatstack.com>
2016-12-20 15:21:12 +0300

    Add rbenv env variable to whenever config

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,6 @@
+set :rbenv_root, "/home/deploy/.rbenv"
+env :PATH, "#{rbenv_root}/shims:#{rbenv_root}/bin:/bin:/usr/bin"
+
 set :output, "/home/deploy/apps/pairup-api/current/log/cron_log.log"
 
 every 1.hour do
