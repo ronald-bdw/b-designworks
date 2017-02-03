23c8d16cdb403caf7147e8b8d17bfb877ddab5bf
Ildus Sadykov <ildus.sadykov@flatstack.com>
2017-02-03 08:43:49 +0300

    import user activities for last 3 days

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,3 +6,7 @@ set :output, "/home/deploy/apps/pairup-api/current/log/cron_log.log"
 every 1.hour do
   rake "activites:import"
 end
+
+every 1.day, at: "12:00am" do
+  rake "activites:import_last_3_days"
+end
