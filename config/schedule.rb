3e9d0f428ce549d0a2cd0fe2ecf472e7800b59ef
dmitry-fedorov <dmitry.fedorov@flatstack.com>
2017-03-28 12:19:24 +0300

    add notifications to scheduller

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,8 +5,10 @@ set :output, "/home/deploy/apps/pairup-api/current/log/cron_log.log"
 
 every 1.hour do
   rake "activites:import"
+  rake "notify_about_subscription_expiration:after_24_hours"
 end
 
 every 1.day, at: "12:00am" do
   rake "activites:import_last_3_days"
+  rake "notify_about_subscription_expiration:after_10_days"
 end
