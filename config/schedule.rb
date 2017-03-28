6aa95610518bf99832836ef6df94c1e513ae24db
dmitry-fedorov <dmitry.fedorov@flatstack.com>
2017-03-28 17:09:47 +0300

    fix task invokation in scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,10 +5,10 @@ set :output, "/home/deploy/apps/pairup-api/current/log/cron_log.log"
 
 every 1.hour do
   rake "activites:import"
-  rake "notify_about_subscription_expiration:after_24_hours"
+  rake "notify_about:urgent_subscription_expiration"
 end
 
 every 1.day, at: "12:00am" do
   rake "activites:import_last_3_days"
-  rake "notify_about_subscription_expiration:after_10_days"
+  rake "notify_about:subscription_expiration"
 end
