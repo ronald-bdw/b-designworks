e95e4576b1986de63b963f234dbfdae46e6a2a33
dmitry-fedorov <dmitry.fedorov@flatstack.com>
2017-03-28 17:01:36 +0300

    move users selection to query object

--- app/interactors/users/notify_about_subscription_expiration.rb
+++ app/interactors/users/notify_about_subscription_expiration.rb
@@ -3,21 +3,14 @@ module Users
     include Interactor
 
     def call
-      users.each do |user|
+      AndroidSubscriptionExpirationUsers.new(expires_at).all.each do |user|
         AndroidSubscriptionsMailer.subscription_expiration(user).deliver_later
       end
     end
 
     private
 
-    def users
-      User
-        .includes(:subscription)
-        .where.not(subscriptions: { id: nil, active: false })
-        .where(device: "android", subscriptions: { expires_at: expires_after })
-    end
-
-    def expires_after
+    def expires_at
       (Time.now + 10.days).beginning_of_day..(Time.now + 10.days).end_of_day
     end
   end
--- app/interactors/users/urgent_notify_about_subscription_expiration.rb
+++ app/interactors/users/urgent_notify_about_subscription_expiration.rb
@@ -3,21 +3,14 @@ module Users
     include Interactor
 
     def call
-      users.each do |user|
+      AndroidSubscriptionExpirationUsers.new(expires_at).all.each do |user|
         AndroidSubscriptionsMailer.urgent_subscription_expiration(user).deliver_later
       end
     end
 
     private
 
-    def users
-      User
-        .includes(:subscription)
-        .where.not(subscriptions: { id: nil, active: false })
-        .where(device: "android", subscriptions: { expires_at: expires_after })
-    end
-
-    def expires_after
+    def expires_at
       (Time.now + 1.days)..(Time.now + 1.days + 1.hour)
     end
   end
