9a08ca2f0433cd0fdda28d152eb93dfd2526419d
RuslanKhabibullinFs <ruslan.khabibullin@flatstack.com>
2017-05-03 13:40:02 +0300

    fix organisation reloads on zendesk

--- spec/support/zendesk_stubs.rb
+++ spec/support/zendesk_stubs.rb
@@ -10,4 +10,10 @@ module ZendeskStubs
 
     allow(ZENDESK_CLIENT).to receive(:organizations).and_return(organizations)
   end
+
+  def stub_zendesk_user_update(user = nil)
+    user ||= FactoryGirl.build(:user)
+    zendesk_users = ZENDESK_CLIENT.users
+    allow(zendesk_users).to receive(:update!).and_return(user)
+  end
 end
